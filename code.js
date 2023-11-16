cec4f64e981710566666ff2569c97e5bade2b8d9
Allison Meadors <allisonmeadors@Allisons-MacBook-Pro.local>
2023-11-16 15:04:13 -0600

    Added html file. Added js file with code for returning order to user in table form.

---
+++ code.js
@@ -0,0 +1,13 @@
+// 1. Prompt user to input "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+//prompt("Welcome to Danny's Froyo!");
+let order = prompt("Welcome to Danny's Froyo! Enter your order, separated by commas.")
+// 2. Store input as an array
+let orderArr = order.split(",");
+// 3. Use loop to determine number of repeat flavors
+const counts = {};
+orderArr.forEach(function(i) {
+    counts[i] = (counts[i] || 0) + 1; 
+});
+// 4. Store number of orders for each flavor as an object
+// 5. Return data to user in a table
+console.table(counts);
